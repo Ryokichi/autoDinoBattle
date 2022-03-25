447d0b9ecce856033420c50a0fd09c49a459f51a
Ryokichi <heitor.nakamura@hotmail.com>
2022-03-25 20:23:45 -0300

    primeiro commit

---
+++ src/Classes/000_Dino.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func beforeAttack():
+	pass
+func onBirth():
+	pass
+func onDeath():
+	pass
+func onAttack():
+	pass
+func onEnemyDeath():
+	pass
+func onHit():
+	pass
+func onKill():
+	pass
+func onPowerUp():
+	pass
+func onSell():
+	pass
+func onTurnEnd():
+	pass
+func onTurnStart():
+	pass
