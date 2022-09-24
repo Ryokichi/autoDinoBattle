5d23031845d6117f98e967cf2511986357f0f870
Sinuva <sinuva_sinuva@hotmail.com>
2022-09-24 19:46:17 -0300

    changes to preCombatScreen

--- src/screens/PreCombatScreen.gd
+++ src/screens/PreCombatScreen.gd
@@ -5,6 +5,13 @@ var dinosInMarket  = []
 var dinosInReserve = []
 var dinosInBattle  = []
 
+var numOfDinosInMarket = 4;
+var currentLevel = 0;
+var dinoTableName = "dinos";
+
+var moneyPerTurn = 10;
+var currentMoney = 0;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,48 +20,98 @@ func _ready():
 	$Background/turn.text = "Turno:" + str(GlobalParams.gameTurn)
 	pass
 
+func onTurnStart():
+	self.currentMoney = self.moneyPerTurn
+	
+	self.interactWithTeam('onTurnStart')
+	
+	pass
 
 func _on_btn_roll_pressed():
 	for dino in dinosInMarket:
 		print ("_on_btn_roll_pressed: ", dino.queue_free())
 		pass
+	
 	dinosInMarket.clear()
 	
-	var tableName = "dinos"
-	db.query("select * from " + tableName + ";")
+	for n in range(0, numOfDinosInMarket):
+		randomize()
+		var rand = stepify(rand_range(0, 100), 1) #Aqui ele só faz um random de 0 a 99(incluso) e força o valor a base inteira(remove qqr coisa pos 1.xxx)
+		var randomizedRarity = self.getRandomizedRarityForDino(rand);
+		
+		var possibilities = db.query("select * from " + self.dinoTableName + " WHERE rarity = " + randomizedRarity + ";").query_result
+		
+		randomize()
+		rand = stepify(rand_range(0, possibilities.length), 1) #Novamente só gera um random, mas agora de 0 até os valores possiveis de dino no lvl selecionado
+		var dino = self.addDinoToMarket(possibilities[rand])
+		var slot = str2var('$slot_market_0' + n as String)
+		dino.slot = slot;
+		dino.set_global_position(slot.get_global_position())
+		pass
+	
+	#Caso você adicione alguma interação de ao jogador dar reroll
+	#self.interactWithTeam('onReroll')
+	
 #	for i in range(0, db.query_result.size()):
 #		print (db.query_result[i])
 #
 #		pass
-	addDinosToMarket(db.query_result)
 	pass
 
-func _on_btn_sell_pressed():
+func getRandomizedRarityForDino(randValue):
+	var chancesOptions = db.query("select " + "LevelDoJogador" + " from chancesPerLevel;").query_result;#modifica o tablename pra alguma que você crie. 
+	# Formato da tabela sugerido
+	# id | lvl1 | lvl2 | lvl3 | lvl4 | lvl5
+	# 0  | 100  |  0   |   0  |  0   |  0
+	# 1  | 70   |  30  |   0  |  0   |  0
+	# 2  | 30   |  40  |   30 |  0   |  0
+	# 3  | 20   |  20  |   30 |  30  |  0
+	# 4  | 10   |  15  |   25 |  20  |  30
+	# É uma sugestão. É possivel ter mais do que isso caso queira que não seja atrelado ao lvl atual do jogador as chances.
+	
+	var cumulativeValue = 0
+	for n in range(0, chancesOptions.length, 1):
+		cumulativeValue += chancesOptions[n]
+		if (cumulativeValue < randValue):
+			return n
+
+func _on_btn_sell_pressed(): #Sugiro não ser um botão e sim uma area de colisão apenas. Ao dropar um dino aqui ele deletar aquele dino e dar dinheiro a você
 	
 	pass
 
+func onDinoSell(dino):
+	var slot = dino.slot;
+	slot.isEmpty = true;
+	
+	self.interactWithTeam('onDinoSold', dino);
+	
+	var money = dino.moneyValue;
+	self.currentMoney += money;
+	
+	#não sei tirar ainda dino do parent e remover do grupo que ta mostrando na tela.
+	pass
 
 func _on_btn_battle_pressed():
+	self.interactWithTeam('beforeBattleStart');
 	var sceneCode = get_tree().change_scene("res://screens/CombatScreen.tscn")
 	print ("Codigo troca de cena: ", sceneCode)
 	pass
 
 
-func addDinosToMarket(list):
-	var dinos = [] + list
-	dinos.shuffle()
+func addDinoToMarket(dinoInfo):
 	
-	for i in range(0, 4):
-		print(str(i) + " addDinosToMarket: res://dinos/"+dinos[i].name+".tscn")
-		dinosInMarket.append(load("res://dinos/"+dinos[i].name+".tscn").instance())
-		pass
+	print(" addDinosToMarket: res://dinos/"+dinoInfo.name+".tscn")
+	var dino = load("res://dinos/"+dinoInfo.name+".tscn").instance()
+	#
+	#dino.hp = dinoInfo.hp #adicionar aqui tudo que seria pra instanciar os stats dos dinos
+	#
+	dinosInMarket.append(dino)
 	
-	for dino in dinosInMarket:
-		add_child(dino)
-		pass
-	
-	dinosInMarket[0].set_global_position($slot_market_00.get_global_position())
-	dinosInMarket[1].set_global_position($slot_market_01.get_global_position())
-	dinosInMarket[2].set_global_position($slot_market_02.get_global_position())
-	dinosInMarket[3].set_global_position($slot_market_03.get_global_position())
+	add_child(dino)
+	return dino
+	pass
+
+func interactWithTeam(funcToCall, caller = null):
+	for dino in dinosInBattle:
+		dino.call(funcToCall, dinosInBattle, caller)
 	pass
